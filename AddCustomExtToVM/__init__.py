d1e9e76349f540b8b87272eb925fc8c625023952
Josh McCurry <joshmccurry@microsoft.com>
2022-06-07 14:53:35 -0400

    Clean Up

--- AddCustomExtToVM/__init__.py
+++ AddCustomExtToVM/__init__.py
@@ -8,6 +8,7 @@ import os
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
+
     # Derived from https://docs.microsoft.com/en-us/azure/virtual-machines/extensions/custom-script-windows#using-multiple-scripts
 
     credentials = DefaultAzureCredential()
@@ -15,23 +16,52 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
     compute_client = ComputeManagementClient(credentials, subscription_id)
 
-    rg_name = '<resource-group>'
+    #Exchange for hardcoded variable, received from GET Parameters/Query or POST body, or even as a Environmental variable from App Settings
+    resource_group_name = '<resource-group>'        
     vm_name = '<vm-name>'
-    extenstion_name = '<vm-script-name>'
-    parameters = {
+    vm_extension_name = '<vm-script-name>'
+
+    #Parameters found here: https://docs.microsoft.com/en-us/python/api/azure-mgmt-compute/azure.mgmt.compute.v2018_10_01.models.virtualmachineextension?view=azure-python
+    # VirtualMachineExtension(*, 
+    #     location: str, 
+    #     tags: Optional[Dict[str, str]] = None, 
+    #     force_update_tag: Optional[str] = None, 
+    #     publisher: Optional[str] = None, 
+    #     type_properties_type: Optional[str] = None, 
+    #     type_handler_version: Optional[str] = None, 
+    #     auto_upgrade_minor_version: Optional[bool] = None, 
+    #     settings: Optional[Any] = None, 
+    #     protected_settings: Optional[Any] = None, 
+    #     instance_view: Optional[azure.mgmt.compute.v2018_10_01.models._models_py3.VirtualMachineExtensionInstanceView] = None, 
+    #     **kwargs)
+    extension_parameters = {
         'location':'eastus',
         'publisher': 'Microsoft.Compute',
         'virtual_machine_extension_type': 'CustomScriptExtension',
         #Types found here: https://github.com/MicrosoftDocs/azure-docs/blob/main/articles/azure-arc/servers/manage-vm-extensions.md
         'type_handler_version': '1.10',
         'auto_upgrade_minor_version': True,
-        'settings':{'fileUris': ["https://xxxxxxx.blob.core.windows.net/buildServer1/1_Add_Tools.ps1"]},
-
+        'settings':{
+            'fileUris': ["https://xxxxxxx.blob.core.windows.net/buildServer1/1_Add_Tools.ps1"]
+        },
+        'protected_settings':{
+            'commandToExecute':"powershell -ExecutionPolicy Unrestricted -File 1_Add_Tools.ps1"
+        }
     }
-    #Parameters found here: https://docs.microsoft.com/en-us/python/api/azure-mgmt-compute/azure.mgmt.compute.v2018_10_01.models.virtualmachineextension?view=azure-python
-    poller = compute_client.virtual_machine_extensions.begin_create_or_update(rg_name, vm_name, extenstion_name, parameters)
-    #Method found here: https://docs.microsoft.com/en-us/python/api/azure-mgmt-compute/azure.mgmt.compute.v2018_10_01.operations.virtualmachineextensionsoperations?view=azure-python
+
+    #Method found here: https://docs.microsoft.com/en-us/python/api/azure-mgmt-compute/azure.mgmt.compute.v2018_10_01.operations.virtualmachineextensionsoperations?view=azure-python#azure-mgmt-compute-v2018-10-01-operations-virtualmachineextensionsoperations-begin-create-or-update
+    # begin_create_or_update(
+    #     resource_group_name: str, 
+    #     vm_name: str, 
+    #     vm_extension_name: str, 
+    #     extension_parameters: _models.VirtualMachineExtension, #See the link above for parameters
+    #     **kwargs: Any) -> LROPoller[_models.VirtualMachineExtension]
+    result = compute_client.virtual_machine_extensions.begin_create_or_update(resource_group_name, vm_name, vm_extension_name, extension_parameters).result()
+
+    logging.info(f'Extension {vm_extension_name} created for {vm_name}.')
+    logging.info(f'{format(result)}')
+
     return func.HttpResponse(
-            "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+            f"This HTTP triggered function executed successfully.\n{format(result)}",
             status_code=200
     )
